f24db96ef990725ba68837e2f2988d5936ddd766
Wyfy0107 <nguyenduy010798@gmail.com>
2020-06-28 14:35:45 +0300

    Updates

---
+++ precache-manifest.25c9b47a9369f7f806edb7507808c5fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8db5d4ccf4095ba112e68e4ca9b9da5",
+    "url": "/%7BDeploy-test%7D/index.html"
+  },
+  {
+    "revision": "39acf659f8986d1a2a06",
+    "url": "/%7BDeploy-test%7D/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c81194e024c7abf269d1",
+    "url": "/%7BDeploy-test%7D/static/js/2.2ba8455b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BDeploy-test%7D/static/js/2.2ba8455b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39acf659f8986d1a2a06",
+    "url": "/%7BDeploy-test%7D/static/js/main.dcccad34.chunk.js"
+  },
+  {
+    "revision": "d74b0a1956c8f52a0040",
+    "url": "/%7BDeploy-test%7D/static/js/runtime-main.a122ac41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/%7BDeploy-test%7D/static/media/logo.5d5d9eef.svg"
+  }
+]);
